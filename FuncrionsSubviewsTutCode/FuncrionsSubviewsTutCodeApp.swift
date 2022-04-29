80fb6049b0cce136f5e5c346c9f5cb9c89aef037
Kazimierz Osinski <kazimierz.osinski@gmail.com>
2022-04-29 10:55:43 +0200

    Initial Commit

---
+++ FuncrionsSubviewsTutCode/FuncrionsSubviewsTutCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuncrionsSubviewsTutCodeApp.swift
+//  FuncrionsSubviewsTutCode
+//
+//  Created by HAL-9001 on 29/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct FuncrionsSubviewsTutCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
